7bbe495639857a93d434f481ca4ea8744d2e4aad
Eric Paul <ericallenpaul@hotmail.com>
2017-09-18 19:07:56 -0400

    initial changes

---
+++ Lunch/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lunch.App_Start
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+    
+}
